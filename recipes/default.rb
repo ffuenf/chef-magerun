daf8892dda118995c0ed56eb5dea64c8a2ba7b78
Achim Rosenhagen <a.rosenhagen@ffuenf.de>
2015-12-12 00:58:10 +0100

    use remote_file resource instead of execute

--- recipes/default.rb
+++ recipes/default.rb
@@ -2,21 +2,15 @@
 # n98-magerun #
 # # # # # # # #
 include_recipe 'php'
-package 'wget'
 
 directory node['n98-magerun']['dir']
-execute 'install_n98-magerun' do
-  cwd node['n98-magerun']['dir']
-  command "wget #{node['n98-magerun']['url']} -O n98-magerun #{node['n98-magerun']['install_dir']}/#{node['n98-magerun']['install_file']}"
-  not_if { ::File.exist?("#{node['n98-magerun']['install_dir']}/#{node['n98-magerun']['install_file']}") }
+remote_file "#{node['n98-magerun']['install_dir']}/#{node['n98-magerun']['install_file']}" do
+  source node['n98-magerun']['url']
+  use_last_modified true
+  owner 'root'
+  group 'root'
+  mode 0755
   creates "#{node['n98-magerun']['install_dir']}/#{node['n98-magerun']['install_file']}"
-  action :run
-  notifies :run, 'execute[make n98-magerun executable]'
-end
-execute 'make n98-magerun executable' do
-  cwd node['n98-magerun']['dir']
-  command "chmod +x #{node['n98-magerun']['install_dir']}/#{node['n98-magerun']['install_file']}"
-  action :nothing
 end
 template '/etc/n98-magerun.yaml' do
   source 'config.yaml.erb'
